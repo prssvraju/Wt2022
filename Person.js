2b7aa3dd69618f1ead204c8af5e59cb15af9720f
2020 <prssvraju2@gmail.com>
2022-03-27 16:18:54 +0530

    update person

---
+++ Person.js
@@ -0,0 +1,9 @@
+module.exports = function(firstname,lastname){
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.fullname=function()
+    {
+        return this.firstname+" "+this.lastname;
+    }
+
+}
