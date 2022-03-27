3c39b3d6bdf07923806c3f15f3513c585042d084
2020 <prssvraju2@gmail.com>
2022-03-27 21:12:47 +0530

    events update

--- url/urlnode.js
+++ url/urlnode.js
@@ -1,8 +1,8 @@
 var http=require("http");
 var url = require('url');
 http.createServer(function(req,res){
-    //var realUrl = (req.connection.encrypted ? 'https': 'http') + '://' + req.headers.host + req.url;
-    var q = url.parse(req.url, true);
+    var realUrl = (req.connection.encrypted ? 'https': 'http') + '://' + req.headers.host + req.url;
+    var q = url.parse(realUrl, true);
     res.write("<h1>Host : </h1>"+q.host+"<br>"); //returns 'localhost:8080'
     res.write("<h1>pathname : </h1>"+q.pathname); //returns '/default.htm'
     res.write("<h1>Search : </h1>"+q.search); //returns '?year=2017&month=february'
